b0e8998f12d8129ff4849cbc1c7cdaa10a8bedfc
taylynnharman <dyer.taylynn@gmail.com>
2024-06-01 21:55:22 -0600

    Error Catching

--- utilities/index.js
+++ utilities/index.js
@@ -24,8 +24,6 @@ Util.getNav = async function (req, res, next) {
   return list;
 };
 
-module.exports = Util;
-
 /* **************************************
  * Build the classification view HTML
  * ************************************ */
@@ -78,3 +76,13 @@ Util.buildClassificationGrid = async function (data) {
   }
   return grid;
 };
+
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = (fn) => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(next);
+
+module.exports = Util;
